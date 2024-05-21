ed4a92bc30bfd46ddfe75b39e3a42cad37a8cf13
Carolina Neponucena <cneponucena@gmail.com>
2024-05-21 11:37:05 -0300

    falta puts

--- html/landing.js
+++ html/landing.js
@@ -1,42 +1,55 @@
-import {getFilmes} from '../js/filmes.js'
+import { getFilmes } from '../js/filmes.js'
 
-const listaFilmes = await getFilmes()
+async function main() {
+  const listaFilmes = await getFilmes()
 
-function criarCard(info){
+  function criarCard(info) {
     const container = document.getElementById('containerRow')
 
     const divImg = document.createElement('div')
-    divImg.classList.add('col-md-2')
+    divImg.classList.add('col-md-2', 'mb-4')
 
     const img = document.createElement('img')
     img.classList.add('card-img')
     img.src = info.foto_capa
 
-    img.setAttribute('data-bs-toggle',"modal")
-    img.setAttribute('data-bs-target',"#modalExemplo")
+    img.setAttribute('data-bs-toggle', 'modal')
+    img.setAttribute('data-bs-target', '#modalExemplo')
     divImg.replaceChildren(img)
     container.appendChild(divImg)
 
-    divImg.addEventListener('click',()=>{
-        abrirFilme(info)
+    divImg.addEventListener('click', () => {
+      abrirFilme(info)
     })
-    
-}
+  }
 
-listaFilmes.forEach(filme => {
+  listaFilmes.forEach(filme => {
+    console.log(filme)
     criarCard(filme)
-});
+  })
 
-function abrirFilme(info){
+  function abrirFilme(info) {
     console.log(info)
     const imagem = document.getElementById('imagemFilme')
     imagem.src = info.foto_capa
 
     const sinopse = document.getElementById('sinopse')
     sinopse.textContent = info.sinopse
-    sinopse.style.fontSize='30px'
+    sinopse.style.fontSize = '30px'
+
+    const titulo = document.getElementById('tituloFilme')
+    titulo.textContent = info.nome
+
+    const valor = document.getElementById('valorFilme')
+    valor.textContent = `Valor: R$${info.valor_unitario}`
+    valor.style.fontSize = '30px'
+  }
+
+  document.getElementById('logo').addEventListener('click', () => {
+    window.location.href = './admin.html'
+  })
 }
 
-document.getElementById('logo').addEventListener('click',()=>{
-    window.location.href='./admin.html'
-})
+main()
+
+
